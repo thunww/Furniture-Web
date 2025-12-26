4174844d02df0e430e839519e73327c0604da67b
Tran Gia Than <trangiathandz2004@gmail.com>
2025-12-26 11:12:57 +0700

    fix jwt

--- Back-end/src/config/jwt.js
+++ Back-end/src/config/jwt.js
@@ -1,6 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = process.env.JWT_SECRET || "your_secret_key";
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+const SECRET_KEY = process.env.JWT_SECRET;
 const EXPIRES_IN = process.env.JWT_EXPIRES_IN || "2h"; // access token mặc định 2h
 
 // Tạo token
